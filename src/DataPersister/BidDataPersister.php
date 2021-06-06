127dd5ca4707759feeaa9ece7fb039842891e4b7
Zakaria Guemi <zakariaguemi18@gmail.com>
2021-06-06 23:02:00 +0100

    add pusher  real time for bids

--- src/DataPersister/BidDataPersister.php
+++ src/DataPersister/BidDataPersister.php
@@ -9,6 +9,7 @@ use App\Entity\Bid;
 use App\Entity\User;
 use App\Repository\BidRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Pusher\Pusher;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Security;
 
@@ -19,13 +20,15 @@ class BidDataPersister implements ContextAwareDataPersisterInterface
     private BidRepository $bidRepository;
     private EntityManagerInterface $entityManager;
     private Security $security;
+    private Pusher $pusher;
 
-    public function __construct(BidRepository $bidRepository, EntityManagerInterface $entityManager,Security $security)
+    public function __construct(BidRepository $bidRepository, EntityManagerInterface $entityManager, Security $security, Pusher $pusher)
     {
         $this->bidRepository = $bidRepository;
         $this->entityManager = $entityManager;
 
         $this->security = $security;
+        $this->pusher = $pusher;
     }
 
     public function supports($data, array $context = []): bool
@@ -35,37 +38,40 @@ class BidDataPersister implements ContextAwareDataPersisterInterface
 
     public function persist($data, array $context = []): Response
     {
-      if($this->checkUser($data)){
-          if($this->isOfferExpired($data)){
-              return new Response(json_encode(['message' => "Offer Ended"]), 401);
-          }
-          if( $this->checkOwnerIsBuyer($data)){
-              return new Response(json_encode(['message' => "Operation Not Possible"]), 401);
-          }
-          if ($data->getPrice() > $data->getProduct()->getCurrentPrice() + $data->getProduct()->getMaxBidPrice() ||
-              $data->getPrice() < $data->getProduct()->getCurrentPrice() + $data->getProduct()->getMinBidPrice()) {
-              return new Response(json_encode(['message' => "Bid price must be enter  " . ($data->getProduct()->getCurrentPrice() + $data->getProduct()->getMinBidPrice()) .
-                  " And " . ($data->getProduct()->getCurrentPrice() + $data->getProduct()->getMaxBidPrice())]), 406);
-          }
-          if ($data->getUser()->getBalance() >= $data->getPrice()) {
+        if ($this->checkUser($data)) {
+            if ($this->isOfferExpired($data)) {
+                return new Response(json_encode(['message' => "Offer Ended"]), 401);
+            }
+            if ($this->checkOwnerIsBuyer($data)) {
+                return new Response(json_encode(['message' => "Operation Not Possible"]), 401);
+            }
+            if ($data->getPrice() > $data->getProduct()->getCurrentPrice() + $data->getProduct()->getMaxBidPrice() ||
+                $data->getPrice() < $data->getProduct()->getCurrentPrice() + $data->getProduct()->getMinBidPrice()) {
+                return new Response(json_encode(['message' => "Bid price must be enter  " . ($data->getProduct()->getCurrentPrice() + $data->getProduct()->getMinBidPrice()) .
+                    " And " . ($data->getProduct()->getCurrentPrice() + $data->getProduct()->getMaxBidPrice())]), 406);
+            }
+            if ($data->getUser()->getBalance() >= $data->getPrice()) {
 
-              $this->returnMoney($data);
-              $data->getUser()->setBalance(($data->getUser()->getBalance() - $data->getPrice()));
-              $data->getProduct()->setCurrentPrice($data->getPrice());
-              $this->entityManager->persist($data);
-              $this->entityManager->flush();
-              return new Response(json_encode(['message' => "Bid Has Been Added"]), 201);
-          }
-          return new Response(json_encode(['message' => "Your Balance less than : " . $data->getPrice()]), 406);
-      } else{
-          return new Response(json_encode(['message' => "this operation Not available!!"]), 401);
-      }
+                $this->returnMoney($data);
+                $data->getUser()->setBalance(($data->getUser()->getBalance() - $data->getPrice()));
+                $data->getProduct()->setCurrentPrice($data->getPrice());
+                $this->entityManager->persist($data);
+                $this->entityManager->flush();
+                $pushuerData['username'] = $data->getUser()->getUsername();
+                $pushuerData['price'] = $data->getPrice();
+                $this->pusher->trigger('channel-bid', 'bid-add', $pushuerData);
+                return new Response(json_encode(['message' => "Bid Has Been Added"]), 201);
+            }
+            return new Response(json_encode(['message' => "Your Balance less than : " . $data->getPrice()]), 406);
+        } else {
+            return new Response(json_encode(['message' => "this operation Not available!!"]), 401);
+        }
 
     }
 
     public function checkUser($data): bool
     {
-        if($data->getUser()==$this->security->getUser()){
+        if ($data->getUser() == $this->security->getUser()) {
             return true;
         }
         return false;
@@ -74,7 +80,7 @@ class BidDataPersister implements ContextAwareDataPersisterInterface
     public function isOfferExpired($data): bool
     {
         if (new \DateTime('now') > $data->getProduct()->getEndedAt()) {
-          return true;
+            return true;
         }
         return false;
     }
@@ -82,7 +88,7 @@ class BidDataPersister implements ContextAwareDataPersisterInterface
     public function checkOwnerIsBuyer($data): bool
     {
         if ($data->getUser() == $data->getProduct()->getSeller()) {
-           return true;
+            return true;
         }
         return false;
     }
